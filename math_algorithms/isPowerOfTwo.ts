61b0232cddc1581234ca175cad99a4b7c5069b33
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-10-01 23:09:21 +0300

    determining if the number is a power of two using two approaches: a cycle and a bitwise operator AND

---
+++ math_algorithms/isPowerOfTwo.ts
@@ -0,0 +1,30 @@
+// O(log(n)) - approximate
+// O(1) - best case
+function isPowerOfTwo(num: number): boolean {
+  if (!Number.isInteger(num) || num < 1) return false;
+
+  let dividedNumber = num;
+  let iterationCounter = 0;
+
+  while (dividedNumber !== 1) {
+    // if the number is no longer dividable by two and it's not
+    // one - it's not a power of two
+    if (dividedNumber % 2 !== 0) {
+      console.log(`Iterations: ${iterationCounter}`);
+      return false;
+    }
+    dividedNumber = dividedNumber / 2;
+
+    iterationCounter++;
+  }
+
+  // if a number was divided by two until it became one - it's a power of two
+  console.log(`Iterations: ${iterationCounter}`);
+  return true;
+}
+
+// should return true
+console.log("Cycle 16: " + isPowerOfTwo(16));
+
+// should return false
+console.log("Cycle 45: " + isPowerOfTwo(45));
---
+++ math_algorithms/isPowerOfTwo_Bitwise.ts
@@ -0,0 +1,19 @@
+// O(1)
+function isPowerOfTwo_Bitwise(num: number):boolean {
+  if (!Number.isInteger(num) || num < 1) return false;
+
+  // since any power of 2 minus 1 is all ones in binary system, i.e.:
+  // 4 in binary is 0100
+  // 1 in binary is 0001
+  // 4 - 1 = 3 in binary is 0100 - 0001 = 0011
+  // same way the 8 - 1 is 0111 and 16 - 1 is 1111
+  // so if we use AND for num and num - 1 we will get
+  // zeros only if the number is a power of two
+  return (num & (num - 1)) === 0;
+}
+
+// should return true
+console.log('Bitwise 16: ' + isPowerOfTwo_Bitwise(16));
+
+// should return false
+console.log('Bitwise 45: ' + isPowerOfTwo_Bitwise(45));

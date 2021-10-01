25f97ab093c4d6083269b397d2b02a569e00173e
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-10-01 23:13:02 +0300

    finding the smallest number in an array

---
+++ math_algorithms/smallestNumberInArray.ts
@@ -0,0 +1,12 @@
+// O(n)
+function smallestNumberInArray(array: number[]) {
+  let smallestNumber = array[0];
+
+  array.map((num) => {
+    if (num < smallestNumber) smallestNumber = num;
+  });
+
+  return smallestNumber;
+}
+
+console.log(smallestNumberInArray([10, 5, 235, 7, -5]));

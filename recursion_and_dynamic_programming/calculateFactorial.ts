603dd2f8d729178779b38bb7f39c162cd5b997c5
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-10-01 22:49:52 +0300

    calculate a factorial of a number

---
+++ recursion_and_dynamic_programming/calculateFactorial.ts
@@ -0,0 +1,15 @@
+// O(n)
+function calculateFactorial(num: number): number {
+  if (num === 1) {
+    return 1;
+  } else {
+    // here we are using 'num *' to pass the value through the recursion
+    return num * calculateFactorial(num - 1);
+  }
+}
+
+// should return 6
+console.log("Factorial of 3: " + calculateFactorial(3));
+
+// should return 120
+console.log("Factorial of 5: " + calculateFactorial(5));

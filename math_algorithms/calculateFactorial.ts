aaec9ea258c0c6248d706b0cce800f40673abe60
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-10-01 22:47:11 +0300

    calculate a factorial of a number

---
+++ math_algorithms/calculateFactorial.ts
@@ -0,0 +1,16 @@
+// O(n)
+function calculateFactorial(num: number): number {
+  let factorial = 1;
+
+  for (let i = num; i >= 1; i--) {
+    factorial *= i;
+  }
+
+  return factorial;
+}
+
+// should return 6
+console.log("Factorial of 3: " + calculateFactorial(3));
+
+// should return 120
+console.log("Factorial of 5: " + calculateFactorial(5));

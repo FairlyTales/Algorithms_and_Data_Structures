7d76be27f3bbcaa775378c289da3d455307b304f
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-10-02 05:35:25 +0300

    linear search with cycle approach (updated file and function names)

--- search_algorithms/linearSearch_Cycle.ts
+++ search_algorithms/linearSearch_Cycle.ts
@@ -1,7 +1,7 @@
 // O(1) - best case (searched item is the last in the list)
 // O(n) - average case (we don't know where the searched item is)
 // O(n) - worst case (searched item is the last in the list)
-function findElementPosition_InArray(arr: number[], element: number): string {
+function linearSearch_Cycle(arr: number[], element: number): string {
   for (const el of arr) {
     if (el === element) {
       return `Index of the element is ${arr.indexOf(el)}`;
@@ -13,8 +13,6 @@ function findElementPosition_InArray(arr: number[], element: number): string {
 
 const array = [2, 7, 123, 74, 865, -15, 44];
 
-// should return 3
-console.log(findElementPosition_InArray(array, 74));
+console.log("should return 3: " + linearSearch_Cycle(array, 74));
 
-// should return 6
-console.log(findElementPosition_InArray(array, 44));
+console.log("should return 6: " + linearSearch_Cycle(array, 44));

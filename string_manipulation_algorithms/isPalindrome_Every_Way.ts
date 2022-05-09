e31db8435280985606804aa244b6f48be05b4eba
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-05-09 21:56:02 +0300

    check if the string is a palindrome using Array.every();

---
+++ string_manipulation_algorithms/isPalindrome_Every_Way.ts
@@ -0,0 +1,14 @@
+// check if the string is a palindrome
+
+// O(n)
+function isPalindromeEveryWay(string: string): boolean {
+  return string.split('').every((value, index, array) => {
+      // console.log(value)
+      // console.log(array[array.length - index - 1])
+      return value === array[array.length - index - 1]
+    }
+  )
+}
+
+console.log(isPalindromeEveryWay('asddsa'));
+console.log(isPalindromeEveryWay('qwerty'));
